7d9206836cd5130ae84c9d78685943676a74d527
mbence01 <h048514@stud.u-szeged.hu>
2021-09-25 16:50:52 +0200

    changed AddressController

--- src/Controller/AddressController.php
+++ src/Controller/AddressController.php
@@ -2,20 +2,28 @@
 
 namespace App\Controller;
 
+use App\Repository\AddressRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class AddressController extends AbstractController
 {
     /**
      * @Route("/address", name="address")
      */
-    public function index(): Response
+    public function index(Environment $env, AddressRepository $addrRepo): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/AddressController.php',
-        ]);
+        //$form = $this->createFormBuilder(new Task())
+          //              ->add("Task", TextType::class)
+            //            ->add("Save", SubmitType::class, ["label" => "Create Task"])
+              //          ->getForm();
+
+        return new Response($env->render(
+            'address/address.html.twig', [
+                'addresses' => $addrRepo->findAll(),
+            ]
+        ));
     }
 }
