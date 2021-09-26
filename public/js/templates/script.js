bd0026de5fde20ed37235dd9ba84df68cfb7a4b4
mbence01 <h048514@stud.u-szeged.hu>
2021-09-27 00:22:10 +0200

    update frontend

--- public/js/templates/script.js
+++ public/js/templates/script.js
@@ -1,6 +1,42 @@
+// Route: /address
+
 let urlParams = getParameters();
 
 if(urlParams.length != 0) {
+    if(urlParams[0][0] == "success" && urlParams[0][1] == 0 && urlParams[1][0] == "err") {
+        switch(urlParams[1][1]) {
+            case "fd": {
+                window.alert("Nem töltöttél ki minden mezőt!\nA *-al jelölt mezők kitöltése kötelező!");
+                break;
+            }
+
+            case "te": {
+                window.alert("Az adószám mező kitöltése kötelező céges cím esetén!");
+                break;
+            }
+
+            case "cb": {
+                window.alert("A rögzítéshez el kell fogadnod az általános szerződési feltételeket!");
+                break;
+            }
+
+            case "pc": {
+                window.alert("Az irányítószámban nem szerepelhetnek betűk!");
+                break;
+            }
+
+            case "ph": {
+                window.alert("A telefonszám formátuma nem megfelelő!\nFormátum: +36XXXXXXXXX (csak számok)");
+                break;
+            }
+
+            case "tx": {
+                window.alert("Az adószám formátuma nem megfelelő!\nFormátum: XXXXXXXX-X-XX");
+                break;
+            }
+        }
+    }
+
     if(urlParams[0][0] == "success" && urlParams[0][1] == 1) {
         switch(urlParams[1][1]) { // honnan lett átirányítva
             case "deladdr": {
@@ -17,9 +53,32 @@ if(urlParams.length != 0) {
                 window.alert("Sikeresen módosítottad a számlázási címedet!");
                 break;
             }
+
+            case "neworder": {
+                window.alert("Sikeresen megrendelted a kiválasztott terméket!");
+                break;
+            }
         }
     }
 }
+
+
+
+// Route: /placeorder
+
+function selectChanged() {
+    let e = document.getElementById("form_addrselect");
+
+    if(e.options[e.selectedIndex].value == 0) {
+        document.getElementById("form_container").style.display = "block";
+    } else {
+        document.getElementById("form_container").style.display = "none";
+    }
+}
+
+
+
+// Function for address
 
 function getParameters() {
     let arr = [];
